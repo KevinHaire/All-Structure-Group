03212ffb250b157aca016d6893dafde4fe76a434
Kevin Haire <kevhaire@gmail.com>
2015-09-15 19:05:52 -0400

    mock up of different homepage design

--- views/home.php
+++ views/home.php
@@ -1,19 +1,30 @@
-<section class="sliderSection">
+<!-- <section class="sliderSection">
 	<div class="sliderContain">
 		<h1>Quick, Efficient, Steel Building Erecting</h1>
 		<div id="gallery-1" class="royalSlider rsDefault visibleNearby">
-	    <a class="rsImg" href="images/slider/slide1.jpg" data-rsw="510" data-rsh="383"></a>
-	    <a class="rsImg" href="images/slider/slide2.jpg" data-rsw="510" data-rsh="383"></a>
-	    <a class="rsImg" href="images/slider/slide3.jpg" data-rsw="510" data-rsh="383"></a>
-	    <a class="rsImg" href="images/slider/slide4.jpg" data-rsw="510" data-rsh="383"></a>
-	  </div>
-	  <div class="button">
-	  	<a href="/contact">
+		    <a class="rsImg" href="images/slider/slide1.jpg" data-rsw="510" data-rsh="383"></a>
+		    <a class="rsImg" href="images/slider/slide2.jpg" data-rsw="510" data-rsh="383"></a>
+		    <a class="rsImg" href="images/slider/slide3.jpg" data-rsw="510" data-rsh="383"></a>
+		    <a class="rsImg" href="images/slider/slide4.jpg" data-rsw="510" data-rsh="383"></a>
+		</div>
+		<div class="button">
+			<a href="/contact">
 				<button>Get A Quote Now</button>
 			</a>
 		</div>
 	</div>
-</section>
+</section> -->
+<div class="gradient">
+	<div class="contain">
+		<section class="hero">
+		</section>
+	</div>
+	<div class="button">
+		<a href="/contact">
+			<button>Get A Quote Now</button>
+		</a>
+	</div>
+</div>
 
 <section class="welcomeSection">
 	<div class="contain">
@@ -21,17 +32,6 @@
 			<h1>Welcome To All Structure Group LTD.</h1>
 			<p>All Structure Group LTD. is a steel building erecting company. We are also capable of completing a variety of different projects from foundations and earth works, to several aspects of interior fit ups.  We have approx. 15-20 men on staff depending on the project and have upwards of 40 if the project requires that much man power. All Structure Group LTD. completes work throughout Alberta, BC, Yukon and NWT.</p>
 		</section>
-		<!-- <section class="welcome2">
-			<h1>Services</h1>
-			<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h2>
-			<ul>
-				<li>Pre-Enginered Building Erection</li>
-				<li>Commercial Wall &amp Roof Cladding Installation</li>
-				<li>Precast Wall installation</li>
-				<li>Mechanical &amp Hanger Door Installation</li>
-				<li>Structural Steel Fabrication &amp Pre-engineered Steel Building Supply Services</li>
-			</ul>
-		</section> -->
 	</div>
 </section>
 
