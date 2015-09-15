8b6d45a0e5520f554b8d5d4027af6b4488e1a2e0
Kevin Haire <kevhaire@gmail.com>
2015-09-14 20:25:50 -0400

    setup 404 and htaccess, removed google maps from contact page temporary

--- views/contact.php
+++ views/contact.php
@@ -11,9 +11,9 @@
 		?>
 	</div>
 
-	<div class="mapEmbed">
+	<!-- <div class="mapEmbed">
 		<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2419.4839717134773!2d-111.31581480000001!3d52.66929770000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x530bb9fe6116e5b1%3A0xa563266e93a9323d!2s4601+49+St%2C+Hardisty%2C+AB+T0B+1V0!5e0!3m2!1sen!2sca!4v1419132156523" width="100%" height="400" frameborder="0" style="border:0"></iframe>
-	</div>
+	</div> -->
 
 	<div style="clear:both;"></div>
 
